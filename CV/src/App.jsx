f93f5149873a3a3bb9a1023126cf2de71c6ec492
canofbubly <derekrussellbroden@gmail.com>
2024-09-25 18:34:38 -0500

    Personal info boxes with state

--- CV/src/App.jsx
+++ CV/src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function WholePageLmao(){
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [personal, setPersonal] = useState({
+    firstName: 'Dan',
+    lastName: 'Steely',
+    email: 'pegdansteely@gmail.com',
+    phone: '888-447-5594'})
+
+  function personalFirst(e){
+    setPersonal({
+      ...personal,
+      firstName: e.target.value
+    })
+  }
+  function personalLast(e){
+    setPersonal({
+      ...personal,
+      lastName: e.target.value
+    })
+  }
+  function personalEmail(e){
+    setPersonal({
+      ...personal,
+      email: e.target.value
+    })
+  }
+  function personalPhone(e){
+    setPersonal({
+      ...personal,
+      phone: e.target.value
+    })
+  }
+  const [education, setEducation] = useState({
+    
+  })
+
+  return(
+    <div className="page">
+      <div className="inputs">
+        <input 
+        value={personal.firstName}
+        onChange={personalFirst}
+        />
+        <input 
+        value={personal.lastName}
+        onChange={personalLast}
+        />
+        <input 
+        value={personal.email}
+        onChange={personalEmail}
+        />
+        <input 
+        value={personal.phone}
+        onChange={personalPhone}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
